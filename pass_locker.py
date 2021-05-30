e8b5ba446eb8789260eda07247960cd91ba5e680
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-05-30 06:59:33 +0300

    add codes for the class instance and checks if username exist

--- pass_locker.py
+++ pass_locker.py
@@ -1,1 +1,29 @@
- 
+class Credential:
+     
+     credential_list=[]
+     """
+     class that generates new instance of credential
+     """
+     def __init__(self, username,password):
+         """
+         """
+         self.username=username
+         self.password=password
+
+     def save_credential(self) :
+         """
+         """
+         Credential.credential_list.append(self)
+
+
+     @classmethod
+     def credential_exist(cls,username):
+         """
+         this method will check if the username exists
+         """
+         for credential in cls.credential_list:
+             if credential.username==username:
+                 return True
+             return False       
+
+
