7ef0d8f2875e70efae73370b909cd7445cf5aa1b
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-05-31 09:22:15 +0300

    add codes to make changes on testing

--- pass_locker_test.py
+++ pass_locker_test.py
@@ -1,3 +1,4 @@
+from main import display_data
 from unittest.case import TestCase
 from pass_locker import Credential
 from pass_locker import UserData
@@ -35,51 +36,75 @@ class Testcredential(unittest.TestCase):
         """
         self.new_credentials.save_credential()
         self.assertEqual(len(Credential.credential_list), 1)
+
+
+
+
+
+
+
+
+
+class TestUserData(unittest.TestCase):
+    '''
+    '''
+
+
+    def setUp(self):
+        '''
+        set up structure before every test
+
+        '''
+        self.new_user_data = UserData("facebook","brian","pass1")
+
+
+    def tearDown(self):
+        '''
+        clean up after runnong each test
+        '''
+        UserData.user_data_list = []
+
+
+
+    def test_init(self):
+        '''
+        Test for test case initialization"
+        '''
+        self.assertEqual(self.new_user_data.account_name, "facebook")
+        self.assertEqual(self.new_user_data.account_username, "brian")
+        self.assertEqual(self.new_user_data.account_password, "pass1")
+
+    def test_add_password(self):
+        '''
+        Testing if the new website and password can be saved
+        '''
+        self.new_user_data.create_password()
+        self.assertEqual(len(UserData.user_data_list),1)
+
+
+
+    def test_show_data(self):
+        '''
+        Testing if the data can be displayed.
+        '''
+        self.new_user_data.create_password()
+        test_this = UserData("twitter","brian", "pass1")
+        test_this.create_password()
+
+        found_user_data = UserData("twitter","brian","pass1")
+        self.assertEqual(found_user_data.account_name,test_this.account_name)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
+
     
-
-
-
-
-
-
-    class TestUserData(unittest,TestCase):
-        """
-        """
-
-        def setUp(self):
-            """
-            setting up structure before every test
-            """
-            self.new_user_data = UserData("facebook", "brian", "2827")
-
-        def tearDown(self):
-            """
-            clean up after running each test
-            """
-            UserData.user_data_list = []
-
-        def test_init(self):
-            """
-            test for the case initialization
-            """
-            self.assertEqual(self.new_user_data.account_name, "facebook")
-            self.assertEqual(self.new_user_data.account_username, "brian")
-            self.assertEqual(self.new_user_data.account_password, "2827")
-
-        def test_add_password(self):
-            """
-            test if the new website and password can be saved    
-            """
-            self.new_user_data.create_password()
-            self.assertEqual(len(UserData.user_data_list), 1)
-
-        def test_display_data(self):
-            """
-            Testing if the data can be displayed.
-            """
-            self.new_user_data.create_password()
-            test_this = UserData("twitter","liz", "pass")
-            test_this.create_password()
-
-            found_user_data = UserData.display_user_data("twitter")
-            self.assertEqual(found_user_data.account_name,test_this.account_name)
+ 
