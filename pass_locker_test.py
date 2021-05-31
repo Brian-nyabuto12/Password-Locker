23c65f2cc27789010c1f12561ce6648b7b1b50a2
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-05-31 07:32:49 +0300

    add codes for the test userdata and authentication

--- pass_locker_test.py
+++ pass_locker_test.py
@@ -1,3 +1,4 @@
+from unittest.case import TestCase
 from pass_locker import Credential
 from pass_locker import UserData
 import unittest
@@ -17,4 +18,48 @@ class Testcredential(unittest.TestCase):
         clean up after running each test
         """
         Credential.credential_list = []
+
+
+    def test_init(self):
+        """
+        test for case initialization
+        """
+        self.assertEqual(self.new_credentials.username, "Brian")
+        self.assertEqual(self.new_credentials.password, "pass")
+
+
+
+    def test_authentication(self):
+        """
+        Test for the proper authetication for log in purposes
+        """
+        self.new_credentials.save_credential()
+        self.assertEqual(len(Credential.credential_list), 1)
+    
+
+
+
+
+
+
+    class TestUserData(unittest,TestCase):
+        """
+        """
+
+        def setUp(self):
+            """
+            setting up structure before every test
+            """
+            self.new_user_data = UserData("facebook", "brian", "pass")
+
+        def tearDown(self):
+            """
+            clean up after running each test
+            """
+            UserData.user_data_list = []
+
+        def test_init(self):
+            """
+            test for the case initialization
+            """
             
